f8c1531812e7272c7c8d887ea076538d6abf97de
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-06-18 11:21:15 +0200

    quick sort inplace python v2

---
+++ src/quick-sort/python/quick_sort_in_place_v2.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def quick_sort_recurse(arr: List[int], low_index: int, high_index: int) -> None:
+    if low_index >= high_index:
+        return
+
+    pivot = arr[high_index]
+    final_pivot_index = low_index
+
+    for i in range(low_index, high_index):
+        if arr[i] < pivot:
+            arr[final_pivot_index], arr[i] = arr[i], arr[final_pivot_index]
+            final_pivot_index += 1
+
+    arr[final_pivot_index], arr[high_index] = arr[high_index], arr[final_pivot_index]
+
+    quick_sort_recurse(arr, 0, final_pivot_index - 1)
+    quick_sort_recurse(arr, final_pivot_index + 1, high_index)
+
+
+def quick_sort(arr: List[int]) -> None:
+    quick_sort_recurse(arr, 0, len(arr) - 1)
+
+
+if __name__ == "__main__":
+    arr = [19, 28, 7, 24, 199, 33, 11]
+    print("Original array", arr)
+    quick_sort(arr)
+    print("Sorted array", arr)
