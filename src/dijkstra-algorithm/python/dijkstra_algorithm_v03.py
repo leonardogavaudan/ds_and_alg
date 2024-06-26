8c7035fcd32eefcf134f22500e43978706cceaf2
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-06-26 14:59:28 +0200

    dijkstra algorithm shortest path python v3

--- src/dijkstra-algorithm/python/dijkstra_algorithm_v03.py
+++ src/dijkstra-algorithm/python/dijkstra_algorithm_v03.py
@@ -1,3 +1,50 @@
+from typing import Dict, List, Optional
+
+
+def find_lowest_cost_node(
+    node_to_cost: Dict[str, int], nodes_processed: set[str]
+) -> Optional[str]:
+    min_node = None
+    min_cost = None
+    for node, cost in node_to_cost.items():
+        if (min_cost is None or min_cost > cost) and node not in nodes_processed:
+            min_node = node
+            min_cost = cost
+    return min_node
+
+
+def dijkstra_shortest_path(
+    graph: Dict[str, Dict[str, int]], starting_node: str, target_node: str
+) -> Optional[List[str]]:
+    node_to_cost = {starting_node: 0}
+    node_to_parent_node: Dict[str, Optional[str]] = {starting_node: None}
+    nodes_processed = set()
+    current_node = starting_node
+    while current_node:
+        for neighbor_node in graph[current_node].keys():
+            if (neighbor_node not in node_to_cost) or (
+                node_to_cost[current_node] + graph[current_node][neighbor_node]
+                < node_to_cost[neighbor_node]
+            ):
+                node_to_cost[neighbor_node] = (
+                    node_to_cost[current_node] + graph[current_node][neighbor_node]
+                )
+                node_to_parent_node[neighbor_node] = current_node
+        nodes_processed.add(current_node)
+        current_node = find_lowest_cost_node(node_to_cost, nodes_processed)
+
+    if target_node not in node_to_cost:
+        return
+
+    current_node = target_node
+    reverse_short_path = []
+    while current_node:
+        reverse_short_path.append(current_node)
+        current_node = node_to_parent_node[current_node]
+
+    return reverse_short_path[::-1]
+
+
 if __name__ == "__main__":
     graph = {
         "A": {"B": 1, "C": 4},
