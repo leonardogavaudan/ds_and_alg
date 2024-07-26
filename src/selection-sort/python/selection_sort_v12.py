e5022d247c565cabb307689e87c8a5e00ae1b10a
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-07-26 10:42:36 +0200

    selection sort python v12

---
+++ src/selection-sort/python/selection_sort_v12.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def selection_sort(arr: List[int]) -> None:
+    for i in range(len(arr) - 1):
+        min_index = i
+        for k in range(i + 1, len(arr)):
+            if arr[k] < arr[min_index]:
+                min_index = k
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+if __name__ == "__main__":
+    arr = [64, 25, 12, 22, 11]
+    print("Original array:", arr)
+    selection_sort(arr)
+    print("Sorted array:", arr)
