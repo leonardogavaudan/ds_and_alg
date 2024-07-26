ccf5ef3e99d9a1d61b07f46eac2d428fe2931ae8
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-07-26 11:34:16 +0200

    bfs traversal python v7

---
+++ src/bfs/python/bfs_traversal_v07.py
@@ -0,0 +1,21 @@
+from collections import deque
+from typing import Dict, List
+
+
+def bfs_traversal(graph: Dict[str, List[str]], start_node: str) -> None:
+    queue = deque([start_node])
+    nodes_seen = set()
+
+    while queue:
+        current_node = queue.popleft()
+
+        for neighbor_node in graph[current_node]:
+            if neighbor_node not in nodes_seen:
+                queue.append(neighbor_node)
+
+        print(current_node)
+
+
+if __name__ == "__main__":
+    graph = {"A": ["B", "C"], "B": ["D", "E"], "C": ["F"], "D": [], "E": ["F"], "F": []}
+    bfs_traversal(graph, "A")
