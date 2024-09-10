68c797446211dbc5f2ef7d5aa8c2ddc671af7d1a
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-09-10 10:01:25 +0200

    merge sort python v1

---
+++ src/merge-sort/python/merge_sort_v1.py
@@ -0,0 +1,37 @@
+from typing import List
+
+def merge_sort(arr: List[int]) -> List[int]:
+    if len(arr) == 1:
+        return arr
+
+    left = merge_sort(arr[:len(arr)//2])
+    right = merge_sort(arr[len(arr)//2:])
+
+    res = []
+
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+
+    while i < len(left):
+        res.append(left[i])
+        i += 1
+    while j < len(right):
+        res.append(right[j])
+        j += 1
+
+    return res
+
+
+if __name__ == "__main__":
+    arr = [4, 3, 5, 9, 7, 8, 2]
+    sorted_arr = merge_sort(arr)
+
+    print(f"Original array: {arr}")
+    print(f"Sorted array: {sorted_arr}")
